3868342d03450e647d4f4c97f9f27fbd66dee968
Nikto8962 <Brianmuiruri15@gmail.com>
2022-10-19 18:37:55 +0300

    changes

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -22,7 +22,7 @@ export default function User() {
       try {
         const res = await userRequest.get("/users/find/" + userId);
         setUser(res.data);
-        console.log(res.data);
+        // console.log(res.data);
       } catch {}
     };
     getUser();
